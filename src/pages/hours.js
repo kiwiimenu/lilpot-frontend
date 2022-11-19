c7c6c2eea75868a03019bad0ceac595961dc242d
ruiichen <frankchen99999@gmail.com>
2022-11-19 01:29:22 -0500

    guh

--- src/pages/hours.js
+++ src/pages/hours.js
@@ -2,10 +2,17 @@ import React from 'react'
 import './hours.css'
 import masterbg from '../assets/masterbackground.png';
 
+
 function hours() {
   return (
-    <div className ='Wrapper'>hours
-        <img src={masterbg} alt="" className='cover'/>
+    <div className="Hours">hours
+        <div style={{backgroundImage:`url(${masterbg})`}}>
+            <div className="Maps">
+            <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=G1%20-%20220%20King%20St%20N%20N2J%203Y7,%20Waterloo,%20ON%20N2J%202Y7&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            </div>
+        
+        </div>
+        
     </div>
   )
 }
