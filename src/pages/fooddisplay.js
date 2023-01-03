b84339fed94c4588159091e3d084815768f74e4d
ruiichen <frankchen99999@gmail.com>
2023-01-03 13:02:22 -0500

    guh2

--- src/pages/fooddisplay.js
+++ src/pages/fooddisplay.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import './fooddisplay.css'
 import card from '../assets/meet1.jpg';
+import masterbg from '../assets/masterbackground.png';
 
 function fooddisplay() {
     return (
-        <div className="collage">
-            <div className="row">
-                <div className="column">
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                </div>
-                <div className="column">
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                </div>
-                <div className="column">
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                </div>
-                <div className="column">
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
-                    <img src={card} className="fade_hov" alt='cardwithtext'></img>
+        <div style={{ backgroundImage: `url(${masterbg})` }}>
+            <div className="collage">
+                <div className="row">
+                    <div className="column">
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                    </div>
+                    <div className="column">
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                    </div>
+                    <div className="column">
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                    </div>
+                    <div className="column">
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                        <img src={card} className="fade_hov" alt='cardwithtext'></img>
+                    </div>
                 </div>
             </div>
         </div>
