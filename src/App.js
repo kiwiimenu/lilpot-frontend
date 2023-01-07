75c6ea23f97bff225410d89707a6e50792cb28ba
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-01-06 20:22:33 -0500

    change restaurant text

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import {BrowserRouter as Router, Route, Switch, Link, useHistory} from 'react-ro
 import AboutUs from './pages/AboutUs';
 import Events from './pages/Events';
 import TopBar from './components/topbar';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 function App() {
--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -3,23 +3,38 @@ import masterbg from '../assets/masterbackground.png';
 import RestaurantFood from '../assets/restaurantfood.png'
 import RestaurantPot from '../assets/restaurantpot.png'
 import {AiOutlineArrowRight} from "react-icons/ai";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { useEffect, useState, useRef } from "react";
 import { db } from "../firebase/firebase-config.js";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { async } from "@firebase/util";
+
 
 
 function Restaurant() {
     
     const [textEn, setTextEn] = useState("");
+    const [textFieldText, setTextField] = useState("");
     const docReference = doc(db, "text", "restauranttextcn");
-
+    const valueRef = useRef('');
+    
     useEffect(() => {
         const getText = async () => {
-          const textData = await getDoc(docReference);
-          setTextEn(textData.data().text);
+            const textData = await getDoc(docReference);
+            setTextEn(textData.data().text);
         };
         getText();
-      }, []);
+    }, []);
+    
+    async function submitText() {
+        const docReference = doc(db, "text", "restauranttextcn");
+        await updateDoc(docReference, {
+            text: valueRef.current.value
+          });
+    }
     return (
         <div  className="background-wrapper" style={{backgroundImage:`url(${masterbg})`}}>
             <div className="image-container">
@@ -37,6 +52,18 @@ function Restaurant() {
                     <img src={RestaurantPot} alt=''></img>
                 </div>
             </div>
+            <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '500px', color: "white"},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Change Display" variant="outlined" multiline
+          rows={8} defaultValue={textEn} InputLabelProps={{ shrink: true }} inputProps={{ style: { color: "white" } }} inputRef={valueRef} />
+    </Box>
+    <Button variant="contained" onClick= {submitText}>Submit</Button>
         </div>
     )
 }
