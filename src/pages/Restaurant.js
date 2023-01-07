5867f95a93a54c43bc7f47b51bd957775378da75
Sarina Li <74844953+sarinali@users.noreply.github.com>
2023-01-06 20:34:41 -0500

    Update Restaurant.js

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -17,14 +17,26 @@ import { async } from "@firebase/util";
 function Restaurant() {
     
     const [textEn, setTextEn] = useState("");
+    const [textCn, setTextCn] = useState("")
     const [textFieldText, setTextField] = useState("");
     const docReference = doc(db, "text", "restauranttextcn");
+    const docReferenceCn = doc(db, "text", "restauranttexten");
     const valueRef = useRef('');
+    const valueRefCn = useRef('');
+    // add a ref for chinese
     
     useEffect(() => {
         const getText = async () => {
             const textData = await getDoc(docReference);
             setTextEn(textData.data().text);
+        };
+        getText();
+    }, []);
+
+    useEffect(() => {
+        const getText = async () => {
+            const textData = await getDoc(docReferenceCn);
+            setTextCn(textData.data().text);
         };
         getText();
     }, []);
@@ -34,6 +46,15 @@ function Restaurant() {
         await updateDoc(docReference, {
             text: valueRef.current.value
           });
+        valueRef.current.value = "";
+    }
+
+    async function submitTextCn() {
+        const docReference = doc(db, "text", "restauranttexten");
+        await updateDoc(docReference, {
+            text: valueRefCn.current.value
+          });
+        valueRefCn.current.value = ""
     }
     return (
         <div  className="background-wrapper" style={{backgroundImage:`url(${masterbg})`}}>
@@ -60,10 +81,23 @@ function Restaurant() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Change Display" variant="outlined" multiline
+      <TextField id="outlined-basic" label="Change Display English" variant="outlined" multiline
           rows={8} defaultValue={textEn} InputLabelProps={{ shrink: true }} inputProps={{ style: { color: "white" } }} inputRef={valueRef} />
     </Box>
+
     <Button variant="contained" onClick= {submitText}>Submit</Button>
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '500px', color: "white"},
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Change Display Chinese" variant="outlined" multiline
+          rows={8} defaultValue={textCn} InputLabelProps={{ shrink: true }} inputProps={{ style: { color: "white" } }} inputRef={valueRefCn} />
+    </Box>
+    <Button variant="contained" onClick= {submitTextCn}>Submit</Button>
         </div>
     )
 }
